2e2654a55e49a0498997982d436900e6c3d1b21e
Tibo <Tibowl@users.noreply.github.com>
2020-04-07 16:21:45 +0200

    Add activity

--- src/main.js
+++ src/main.js
@@ -22,9 +22,18 @@ global.config = require("./data/config.json")
 
 const clients = []
 
+const lyrics         = JSON.parse(fs.readFileSync("./data/song.json").toString())
 Object.values(global.config.tokens).forEach(token => {
     const client = new Discord.Client()
-    client.on("ready", () => Logger.info(`${client.user.username} is ready!`))
+    client.on("ready", () => {
+        Logger.info(`${client.user.username} is ready!`)
+
+        for (const {text} of lyrics)
+            if(text.startsWith(client.user.username)) {
+                client.user.setActivity(text.substring(client.user.username.length), {type: "PLAYING"})
+                break
+            }
+    })
     client.login(token)
     clients.push(client)
 })
@@ -35,11 +44,13 @@ clients[0].on("message", async (message) => {
     if(message.author.bot) return
 
     if(message.content == "never gonna give you up") {
+        Logger.info(`${message.author.tag}: ${message.content}`)
         rickRoll(clients)
         return
     }
 
     if(message.content == "never gonna shutdown") {
+        Logger.info(`${message.author.tag}: ${message.content}`)
         clients.forEach(client =>
             setTimeout(() => {
                 client.destroy()
